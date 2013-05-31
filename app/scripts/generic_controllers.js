0e3cae1b47d4a4f21b8bce8922787e26a22681a8
Jo Voordeckers <jo.voordeckers@gmail.com>
2013-05-31 04:58:28 +0200

    Twitter, Tracks, random layout fixes left and right

--- app/scripts/generic_controllers.js
+++ app/scripts/generic_controllers.js
@@ -52,39 +52,96 @@ angular.module('cfpSpeakerApp')
         });
     }).controller('ProfileCtrl',function ($scope, UserService) {
         $scope.model = {};
-//        EventBus.onLoginSuccess($scope, function (user, userToken, event) {
-//            $scope.model.speakerDetails = user;
-//        });
+
+        UserService.waitForCurrentUser().then(function(){
+                $scope.model.speakerDetails = UserService.currentUser;
+        });
+
         $scope.profileComplete = UserService.profileComplete;
 
         $scope.updateProfile = function () {
             UserService.updateProfile($scope.model.speakerDetails);
         };
     }).controller('HomeCtrl',function ($scope, $location) {
+
     }).controller('ContactCtrl',function ($scope, $location) {
     }).controller('AboutCtrl',function ($scope, $location) {
-    }).controller('TwitterCtrl', function ($scope, $location) {
-        $scope.tweets = [
-            {
-                thumbnail: 'images_dummy/tweeter_dummy.jpg',
-                name: '',
-                tweet: '1 I was wondering who is going tonight to the #devoxx seminar in Brussels? Anyone has a ticket left over?',
-                handle: '@Lialee',
-                time: '30 minutes ago'
-            },
-            {
-                thumbnail: 'images_dummy/tweeter_dummy.jpg',
-                name: '',
-                tweet: '2 I was wondering who is going tonight to the #devoxx seminar in Brussels? Anyone has a ticket left over?',
-                handle: '@Lialee',
-                time: '30 minutes ago'
-            },
-            {
-                thumbnail: 'images_dummy/tweeter_dummy.jpg',
-                name: '',
-                tweet: '3 I was wondering who is going tonight to the #devoxx seminar in Brussels? Anyone has a ticket left over?',
-                handle: '@Lialee',
-                time: '30 minutes ago'
+    }).controller('TwitterCtrl', function ($scope, $location, $http, $timeout) {
+        
+        var self = this;
+
+        var MAX = 4;
+        var maxTweetId = 0;
+
+        var tweetQueue = [];
+        $scope.tweets = [];
+        $scope.scrollClass = "";
+
+        this.refreshRemoteData = function() {
+
+            $http.jsonp("http://search.twitter.com/search.json?q=%23devoxx&rpp=10&since_id=" + maxTweetId  
+                + "&include_entities=false&with_twitter_user_id=true&result_type=mixed&callback=JSON_CALLBACK")
+            .success(function(data, status) {
+                    
+                //console.log('HTTP Code: ' + code + ' Data: ' + JSON.stringify(data));
+
+                data.results.forEach(function(result){
+                    var tweet = new Tweet(result);
+                    // Prevent the latest search result from popping up multiple times in our queue if there aren't any new tweets
+                    if (maxTweetId != tweet.id) {
+                        tweetQueue.unshift(tweet);
+                    }
+                });
+
+                maxTweetId = data.max_id;
+
+                if ($scope.tweets.length == 0) {
+                    self.tweetQueueProcessor(); // Populate on init
+                }
+
+            });
+
+            $timeout(self.refreshRemoteData, 10000);
+        }
+
+        this.tweetQueueProcessor = function() {
+            
+            // Initialisation
+            if ($scope.tweets.length < MAX) {
+                while ($scope.tweets.length < MAX && tweetQueue.length > 0) {
+                    $scope.tweets.push(tweetQueue.shift());
+                }
+
             }
-        ];
+
+            // Regular operation
+            else if (tweetQueue.length > 0) {
+                $scope.tweets.push(tweetQueue.shift());
+                $scope.scrollClass = "scrollup";
+                $timeout(shiftTweets, 1900);
+            }
+
+            function shiftTweets() {
+                $scope.tweets.shift();
+                $scope.scrollClass = "";
+            }
+
+            $timeout(self.tweetQueueProcessor, 3000);
+        }
+
+        function Tweet(tweet) {
+
+            this.id = tweet.id;
+            this.author = tweet.from_user;
+            this.image = tweet.profile_image_url;
+            this.tweet = tweet.text;
+            this.time = moment(tweet.created_at, "ddd, DD MMM YYYY HH:mm:ss ZZ").fromNow(); //Thu, 30 May 2013 15:02:41 +0000
+
+            this.toString = function() {
+                return this.author + " " + this.id;
+            }
+        }
+
+        $timeout(self.refreshRemoteData, 0);
+        
     });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,149 +1,174 @@
-$(document).ready(function () {
-    $("select, input[type=file], input[type=password], input[type=text], input[type=checkbox], input[type=radio], textarea").uniform();
+$(document).ready(function() {
 
-    $(".selector select").css("width", "100%");
+	$("select, input[type=file], input[type=text], input[type=checkbox], input[type=radio], textarea").uniform();
+	$(".selector select").css("width","100%");
+	
+	$(window).resize(function() {
+		updateFormElements();
+	}); 
+	
+	
+	$('header nav').meanmenu();
+	
 
-    $('header nav').meanmenu();
+	$('#carousel ul').bxSlider({
+		mode: 'fade',		/* */
+		speed: 500,			/* */
+		auto: true,			/* */
+		minSlides: 1,		/* */
+		maxSlides: 1,		/* */
+		slideMargin: 0,		/* */
+		pause: 4000,		/* timeout between animation */
+		moveSlides: 1,		/* number slides to move */
+		controls: false, 	/* show prev/next */
+		pager: true, 		/* show pager */
+		touchEnabled: false,
+		swipeThreshold: 5,
+		oneToOneTouch: false,
+		tickerHover: true,
+		adaptiveHeight: true
+	});
+	
+	
+	
+	$('#news .carousel ul').bxSlider({
+		mode: 'horizontal',	/* */
+		speed: 500,			/* */
+		auto: false,			/* */
+		minSlides: 1,		/* */
+		maxSlides: 40,		/* */
+		slideMargin: 2,		/* */
+		pause: 4000,		/* timeout between animation */
+		moveSlides: 1,		/* number slides to move */
+		controls: true, 	/* show prev/next */
+		pager: false, 		/* show pager */
+		touchEnabled: false,
+		swipeThreshold: 5,
+		oneToOneTouch: false,
+		slideWidth: 266,
+		tickerHover: true,
+		adaptiveHeight: true
+	});
 
-    $('#carousel ul').bxSlider({
-        mode: 'fade', /* */
-        speed: 500, /* */
-        auto: true, /* */
-        minSlides: 1, /* */
-        maxSlides: 1, /* */
-        slideMargin: 0, /* */
-        pause: 4000, /* timeout between animation */
-        moveSlides: 1, /* number slides to move */
-        controls: false, /* show prev/next */
-        pager: true, /* show pager */
-        touchEnabled: false,
-        swipeThreshold: 5,
-        oneToOneTouch: false,
-        tickerHover: true,
-        adaptiveHeight: true
-    });
 
-    $('#news .carousel ul').bxSlider({
-        mode: 'horizontal', /* */
-        speed: 500, /* */
-        auto: false, /* */
-        minSlides: 1, /* */
-        maxSlides: 40, /* */
-        slideMargin: 2, /* */
-        pause: 4000, /* timeout between animation */
-        moveSlides: 1, /* number slides to move */
-        controls: true, /* show prev/next */
-        pager: false, /* show pager */
-        touchEnabled: false,
-        swipeThreshold: 5,
-        oneToOneTouch: false,
-        slideWidth: 266,
-        tickerHover: true,
-        adaptiveHeight: true
-    });
 
-    $("#tweetwall .carousel ul li:even").addClass("even");
 
-    $('#tweetwall .carousel ul').bxSlider({
-        mode: 'vertical', /* */
-        speed: 500, /* */
-        auto: true, /* */
-        minSlides: 3, /* */
-        maxSlides: 3, /* */
-        slideMargin: 0, /* */
-        pause: 4000, /* timeout between animation */
-        moveSlides: 1, /* number slides to move */
-        controls: false, /* show prev/next */
-        pager: false, /* show pager */
-        touchEnabled: false,
-        swipeThreshold: 50,
-        oneToOneTouch: false,
-        easing: 'ease',
-        tickerHover: true,
-        adaptiveHeight: true
-    });
+	$("#tweetwall .carousel ul li:even").addClass("even");
+	
+	$('#tweetwall .carousel ul').bxSlider({
+		mode: 'vertical',	/* */
+		speed: 500,			/* */
+		auto: true,			/* */
+		minSlides: 3,		/* */
+		maxSlides: 3,		/* */
+		slideMargin: 0,		/* */
+		pause: 4000,		/* timeout between animation */
+		moveSlides: 1,		/* number slides to move */
+		controls: false, 	/* show prev/next */
+		pager: false, 		/* show pager */
+		touchEnabled: false,
+		swipeThreshold: 50,
+		oneToOneTouch: false,
+		easing: 'ease',
+		tickerHover: true,
+		adaptiveHeight: true
+	});
+	
+	
+	
+	
+	
+	$("header nav ul li.submenu").mouseover(function(){
+		$(this).addClass("open");
+		$("header #submenu").stop().animate({height: 45}, 200);
+		$(this).find('ul').stop().fadeIn();
+		
+	}).mouseleave(function() {		
+		$(this).removeClass("open");
+		$(this).find('ul').stop().fadeOut(200);
+		$("header #submenu").animate({height: 0}, 200);		
+	});
+	
+	
+	$("header nav ul li.popup").mouseover(function(){
+		$(this).addClass("open");
+		$(this).find('ul').fadeIn(200);
+		
+	}).mouseleave(function() {		
+		$(this).removeClass("open");
+		$(this).find('ul').fadeOut(200);	
+	});
+	
+	
+	
+	
+		
+	// Fade in images so there isn't a color "pop" document load and then on window load
+	$("footer .sponsors img").fadeIn(200);
+	
+	// clone image
+	$('footer .sponsors img').each(function(){
+		var el = $(this);
+		el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"998","opacity":"0"}).insertBefore(el).queue(function(){
+			var el = $(this);
+			el.parent().css({"width":this.width,"height":this.height});
+			el.dequeue();
+		});
+		this.src = grayscale(this.src);
+	});
+	
+	// Fade image 
+	$('footer .sponsors img').mouseover(function(){
+		$(this).parent().find('img:first').stop().animate({opacity:1}, 200);
+	})
+	$('.img_grayscale').mouseout(function(){
+		$(this).stop().animate({opacity:0}, 500);
+	});
+	
+	
+	
+	
+		
+	// FAQ accordeon
+	$('#accordion .acco_title').click(function() {
+		$('#accordion .acco_content').slideUp('normal');	
+		$(this).next().slideDown('normal');
+	});
+		
+		
+		
 
-    $("header nav ul li.submenu").mouseover(function () {
-        $(this).addClass("open");
-        $("header #submenu").stop().animate({height: 45}, 200);
-        $(this).find('ul').stop().fadeIn();
-    }).mouseleave(function () {
-            $(this).removeClass("open");
-            $(this).find('ul').stop().fadeOut(200);
-            $("header #submenu").animate({height: 0}, 200);
-        });
-
-    $("header nav ul li.popup").mouseover(function () {
-        $(this).addClass("open");
-        $(this).find('ul').fadeIn(200);
-    }).mouseleave(function () {
-            $(this).removeClass("open");
-            $(this).find('ul').fadeOut(200);
-        });
-
-    // Fade in images so there isn't a color "pop" document load and then on window load
-    $("footer .sponsors img").fadeIn(200);
-
-    // clone image
-    $('footer .sponsors img').each(function () {
-        var el = $(this);
-        el.css({"position": "absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position": "absolute", "z-index": "998", "opacity": "0"}).insertBefore(el).queue(function () {
-            var el = $(this);
-            el.parent().css({"width": this.width, "height": this.height});
-            el.dequeue();
-        });
-        this.src = grayscale(this.src);
-    });
-
-    // Fade image
-    $('footer .sponsors img').mouseover(function () {
-        $(this).parent().find('img:first').stop().animate({opacity: 1}, 200);
-    })
-    $('.img_grayscale').mouseout(function () {
-        $(this).stop().animate({opacity: 0}, 500);
-    });
-
-    // FAQ accordeon
-    $('#accordion .acco_title').click(function () {
-        $('#accordion .acco_content').slideUp('normal');
-        $(this).next().slideDown('normal');
-    });
-});
-
-// FAQ accordeon
-$('#accordion .acco_title').click(function() {
-    $('#accordion .acco_content').slideUp('normal');
-    $(this).next().slideDown('normal');
 });
 
 
 // Grayscale w canvas method
-function grayscale(src) {
-    var canvas = document.createElement('canvas');
-    var ctx = canvas.getContext('2d');
-    var imgObj = new Image();
-    imgObj.src = src;
-    canvas.width = imgObj.width;
-    canvas.height = imgObj.height;
-    ctx.drawImage(imgObj, 0, 0);
-    var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    for (var y = 0; y < imgPixels.height; y++) {
-        for (var x = 0; x < imgPixels.width; x++) {
-            var i = (y * 4) * imgPixels.width + x * 4;
-            var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-            imgPixels.data[i] = avg;
-            imgPixels.data[i + 1] = avg;
-            imgPixels.data[i + 2] = avg;
-        }
-    }
-    ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-    return canvas.toDataURL();
+function grayscale(src){
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	var imgObj = new Image();
+	imgObj.src = src;
+	canvas.width = imgObj.width;
+	canvas.height = imgObj.height; 
+	ctx.drawImage(imgObj, 0, 0); 
+	var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	for(var y = 0; y < imgPixels.height; y++){
+		for(var x = 0; x < imgPixels.width; x++){
+			var i = (y * 4) * imgPixels.width + x * 4;
+			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+			imgPixels.data[i] = avg; 
+			imgPixels.data[i + 1] = avg; 
+			imgPixels.data[i + 2] = avg;
+		}
+	}
+	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+	return canvas.toDataURL();
 }
 
-function updateFormElements() {
 
-    $(".selector select").css("width","85%");
-    $.uniform.update();
-    $(".selector select").css("width","100%");
+function updateFormElements() {
+	
+	$(".selector select").css("width","85%");
+	$.uniform.update();
+	$(".selector select").css("width","100%");
 
 }
