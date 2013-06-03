34e88b68861ebf838f7e525c1a7483d0190955ac
Jo Voordeckers <jo.voordeckers@gmail.com>
2013-06-03 10:21:53 +0200

    Fix for logo's

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -99,27 +99,36 @@ $(document).ready(function() {
 	});
 	
 		
+    // Fade in images so there isn't a color "pop" document load and then on window load
+    $("footer .sponsors img").fadeIn(200);
+
 	// Fade in images so there isn't a color "pop" document load and then on window load
-	$("footer .sponsors img").fadeIn(200);
-	
-	// clone image
-	$('footer .sponsors img').each(function(){
-		var el = $(this);
-		el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"998","opacity":"0"}).insertBefore(el).queue(function(){
-			var el = $(this);
-			el.parent().css({"width":this.width,"height":this.height});
-			el.dequeue();
-		});
-		this.src = grayscale(this.src);
-	});
-	
-	// Fade image 
-	$('footer .sponsors img').mouseover(function(){
-		$(this).parent().find('img:first').stop().animate({opacity:1}, 200);
-	})
+	$("footer .sponsors img")
+        .fadeIn(200)
+        .each(function(){
+            // clone image
+            var el = $(this);
+            el.wrap("<span class='img_wrapper'>")
+                .clone()
+                .addClass('img_grayscale')
+                .css({"position":"absolute","z-index":"998","opacity":"0"})
+                .insertBefore(el)
+                .queue(function(){
+                    var el = $(this);
+                    el.parent()
+                      .css({"width":this.width,"height":this.height})
+                      .dequeue();
+                });
+            console.log('el', el[0])
+            grayscale(el[0]);
+        });
+
+    // Fade image in and out on hover
 	$('.img_grayscale').mouseout(function(){
 		$(this).stop().animate({opacity:0}, 500);
-	});
+	}).mouseover(function(){
+        $(this).parent().find('img.img_grayscale').stop().animate({opacity:1}, 200);
+    });
 	
 	// Angular Directive
 	
@@ -133,26 +142,30 @@ $(document).ready(function() {
 
 
 // Grayscale w canvas method
-function grayscale(src){
-	var canvas = document.createElement('canvas');
-	var ctx = canvas.getContext('2d');
-	var imgObj = new Image();
-	imgObj.src = src;
-	canvas.width = imgObj.width;
-	canvas.height = imgObj.height; 
-	ctx.drawImage(imgObj, 0, 0); 
-	var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	for(var y = 0; y < imgPixels.height; y++){
-		for(var x = 0; x < imgPixels.width; x++){
-			var i = (y * 4) * imgPixels.width + x * 4;
-			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-			imgPixels.data[i] = avg; 
-			imgPixels.data[i + 1] = avg; 
-			imgPixels.data[i + 2] = avg;
-		}
-	}
-	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-	return canvas.toDataURL();
+function grayscale(elem){
+	var newImage = new Image();
+	newImage.onload = (function(elem, imgObj) {
+        return function() {
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+            canvas.width = imgObj.width;
+            canvas.height = imgObj.height;
+            ctx.drawImage(imgObj, 0, 0);
+            var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            for(var y = 0; y < imgPixels.height; y++){
+                for(var x = 0; x < imgPixels.width; x++){
+                    var i = (y * 4) * imgPixels.width + x * 4;
+                    var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+                    imgPixels.data[i] = avg;
+                    imgPixels.data[i + 1] = avg;
+                    imgPixels.data[i + 2] = avg;
+                }
+            }
+            ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+            elem.src = canvas.toDataURL();
+        };
+    })(elem, newImage);
+    newImage.src = elem.src;
 }
 
 
