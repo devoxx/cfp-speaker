6ef64e039935f785bfc6c9e166b175e09ed9b78c
Jo Voordeckers <jo.voordeckers@gmail.com>
2013-06-04 11:00:37 +0200

    Cleanup

--- app/scripts/speaker/Proposals.js
+++ app/scripts/speaker/Proposals.js
@@ -21,10 +21,7 @@ speakerModule.controller(speakerCtrlPrefix + 'ProposalsCtrl', function ($scope, 
         if (confirm('Are you sure you want to delete this proposal? This cannot be undone!')) {
             // TalksService.deleteProposal(proposal).success(function(){
                 var proposals = $scope.model.myProposals;
-                console.log(JSON.stringify(proposals));
-
                 proposals.splice(proposals.indexOf(proposal), 1);    
-                console.log(JSON.stringify(proposals));
             // })
         }
     }
