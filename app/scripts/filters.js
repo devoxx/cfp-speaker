a593526c0298ac78af37b217717228c3f4c05ee9
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2013-06-06 19:54:16 +0200

    completed filters tests

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -2,18 +2,24 @@
 
 angular.module('cfpSpeakerApp').filter('spaceSeparatedArray',function () {
     return function (input) {
-        return input.join(' ');
+        if (input && input.join) {
+            return input.join(' ');
+        } else {
+            return '';
+        }
     };
 }).filter('exclude', function () {
-        return function (input, otherArray) {
-            var ret = [];
-            for (var i = 0; i < input.length; i++) {
-                var obj = input[i];
-                console.log('' + otherArray + '.indexOf(' + obj + ') -->' + (otherArray.indexOf(obj) == -1))
-                if (otherArray.indexOf(obj) == -1) {
-                    ret.push(obj);
-                }
-            }
-            return ret;
+    return function (input, otherArray) {
+        var ret = [];
+        if (!input) {
+            throw new Error('input argument is required for "exclude" filter');
         }
-    });
+        for (var i = 0; i < input.length; i++) {
+            var obj = input[i];
+            if (!otherArray || otherArray.indexOf(obj) == -1) {
+                ret.push(obj);
+            }
+        }
+        return ret;
+    }
+});
--- test/spec/controllers/filtersSpec.js
+++ test/spec/controllers/filtersSpec.js
@@ -4,8 +4,59 @@ describe('Filters', function() {
 
     beforeEach(module('cfpSpeakerApp'));
 
-    it('should combine all array elements in one space separated string', inject(function($filter) {
-        var result = $filter('spaceSeparatedArray')(['1', '2', '3']);
-        expect(result).toEqual('1 2 3');
+    var filter;
+    beforeEach(inject(function($filter) {
+        filter = $filter;
     }));
+
+    describe('spaceSeparatedArray', function() {
+        it('should combine all array elements in one space separated string', inject(function() {
+            var result = filter('spaceSeparatedArray')(['1', '2', '3']);
+            expect(result).toEqual('1 2 3');
+        }));
+
+        it('should return an empty string if an empty array is passed in', inject(function() {
+            var result = filter('spaceSeparatedArray')([]);
+            expect(result).toEqual('');
+        }));
+
+        it('should return an empty string if null is passed in', inject(function() {
+            var result = filter('spaceSeparatedArray')();
+            expect(result).toEqual('');
+        }));
+    });
+
+    describe('exclude', function() {
+        it('should return an empty array if the both arguments are empty', function() {
+            var result = filter('exclude')([], []);
+            expect(result).toEqual([]);
+        });
+
+        it('should return an empty array if the first argument is empty', function() {
+            var result = filter('exclude')([], [1,2,3]);
+            expect(result).toEqual([]);
+        });
+
+        it('should return an array equal to the first argument if the second argument is empty', function() {
+            var result = filter('exclude')([1,2,3], []);
+            expect(result).toEqual([1,2,3]);
+        });
+
+        it('should return an array equal to the first argument with the elements from the second array left out', function() {
+            var result = filter('exclude')([1,2,3,4,5], [2,4]);
+            expect(result).toEqual([1,3,5]);
+        });
+
+        it('should not accept the first argument to be null', function() {
+            function test() {
+                filter('exclude')(null, [1,2]);
+            }
+            expect(test).toThrow();
+        });
+
+        it('should return an array equal to the first argument if the second argument is null', function() {
+            var result = filter('exclude')([1,2,3], null);
+            expect(result).toEqual([1,2,3]);
+        });
+    });
 });
