d2274d35ab09210f69f8e49af858e32c0b446bd6
Stephan Janssen <sja@bejug.org>
2013-07-23 12:07:53 +0200

    - Added Thank You page for registration - Added "Buy Tickets"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,8 @@ cfpSpeakerApp.config(function ($routeProvider) {
             templateUrl: 'views/practical/venue.html'
         }).when('/sponsors', {
             templateUrl: 'views/sponsors.html'
+        }).when('/registered', {
+                templateUrl: 'views/thank_you.html'
         }).otherwise({
             redirectTo: '/'
         });
