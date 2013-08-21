b3b7d6deb8a3d53c1d3d3d0fb04a1bd2f90a9293
Jo Voordeckers <jo.voordeckers@gmail.com>
2013-08-21 15:00:14 +0200

    Exclude mobile track icons from versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -286,6 +286,7 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
             '<%= yeoman.dist %>/images/{,**/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '!<%= yeoman.dist %>/images/tracks/mobile/**/*',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
